e05105ea58aa4a7820f19506f8a1f57d6602942a
lioarce01 <lioarce1@gmail.com>
2022-11-01 01:38:21 -0300

    subs done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import List from './components/List';
+import {Sub} from './types'
+
+interface AppState {
+  subs: Sub[],
+  newSubsNumber: number,
+}
 
 function App() {
+  const [subs, setSubs] = useState<AppState["subs"]>([])
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0)
+  const divRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const subs = localStorage.getItem("subs")
+    if (subs) {
+      setSubs(JSON.parse(subs))
+    }
+  }, [])
+
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub])
+    setNewSubsNumber(n => n + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={divRef}>
+      <h1>subs</h1>
+      <List subs={subs} />
+      New subs: {newSubsNumber}
+      <Form onNewSub={handleNewSub}/>
     </div>
   );
 }
